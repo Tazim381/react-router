2ac2512d0d8cca7b1ce3319fcbf484a5f27a44f9
Tazim381 <jucse28.381@gmail.com>
2023-05-11 22:16:31 +0600

    footer added

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="navhome">
-      <div className="navbar">
+      <div className="nav">
         <Link to="/">
           <div>Home</div>
         </Link>
